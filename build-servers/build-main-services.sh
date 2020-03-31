b3f6d270d9665968c477f297fe280c361a1aa956
Zaven <matthew.fullstackgis@gmail.com>
2020-03-30 20:26:06 -0700

    Refactor map and admin api service build into their own jobs

---
+++ build-servers/build-main-services.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/install-python/install-python.yml
+ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/install-dependencies/install-dependencies.yml
+ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/setup-map-api-service/setup-map-api-service.yml
+ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/setup-admin-api-service/setup-admin-api-service.yml
