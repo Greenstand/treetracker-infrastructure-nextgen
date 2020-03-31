eac2c59112dcfd01b8509c67a6ef9f900ec8056a
Zaven <matthew.fullstackgis@gmail.com>
2020-03-31 15:25:18 -0700

    Correct script name for build pipeline

--- build-servers/build-pipeline.sh
+++ build-servers/build-pipeline.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 ansible-playbook  -i ../hosts/$ENV.hosts -l batch --user root  playbooks/install-python/install-python.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l batch --user root  playbooks/install-dependencies/install-dependencies.yml
-ansible-playbook  -i ../hosts/$ENV.hosts -l batch --user root  playbooks/setup-services/setup-services.yml
+ansible-playbook  -i ../hosts/$ENV.hosts -l batch --user root  playbooks/setup-pipeline-services/setup-pipeline-services.yml
