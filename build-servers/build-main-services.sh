b6de9b4abde9d5e27297c00a8e3194874131fa8c
Zaven <matthew.fullstackgis@gmail.com>
2020-03-30 20:47:29 -0700

    Add build step to install nginx

--- build-servers/build-main-services.sh
+++ build-servers/build-main-services.sh
@@ -3,3 +3,4 @@ ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/install-
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/install-dependencies/install-dependencies.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/setup-map-api-service/setup-map-api-service.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/setup-admin-api-service/setup-admin-api-service.yml
+ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/install-nginx/install-nginx.yml
