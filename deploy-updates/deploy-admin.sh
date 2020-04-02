3e7a673579362370d14b49ee81f361ccff3028b1
Zaven <matthew.fullstackgis@gmail.com>
2020-04-01 18:10:51 -0700

    Simplify deploy admin

--- deploy-updates/deploy-admin.sh
+++ deploy-updates/deploy-admin.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
+
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <hosts>"
+  exit
+fi
+ENV=$1
+
+export ANSIBLE_VAULT_PASSWORD_FILE=.vault_pass.txt
 ansible-playbook  -i ../hosts/test.hosts -l main --user root  playbooks/update-admin-api/update-admin-api.yml
 ansible-playbook  -i ../hosts/test.hosts -l main --user root  playbooks/update-admin-client/update-admin-client.yml
--- deploy-updates/deploy-prod-admin.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-ansible-playbook  -i ../hosts/prod.hosts -l main --user root  playbooks/update-admin-api/update-admin-api.yml
-ansible-playbook  -i ../hosts/prod.hosts -l main --user root  playbooks/update-admin-client/update-admin-client.yml
