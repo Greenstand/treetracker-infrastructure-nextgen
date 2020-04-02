9387b892c66db2ad07e546d26ed1db8b280b32b1
Zaven <matthew.fullstackgis@gmail.com>
2020-04-01 19:47:16 -0700

    Ask for env

--- build-servers/build-pipeline.sh
+++ build-servers/build-pipeline.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
+
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <hosts>"
+  exit
+fi
+ENV=$1
+
 ansible-playbook  -i ../hosts/$ENV.hosts -l batch --user root  playbooks/install-python/install-python.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l batch --user root  playbooks/install-dependencies/install-dependencies.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l batch --user root  playbooks/setup-pipeline-services/setup-pipeline-services.yml
