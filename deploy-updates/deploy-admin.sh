848636f17f7632337f2bcbdb553389c7b91aae10
Zaven <matthew.fullstackgis@gmail.com>
2020-05-06 11:19:58 -0700

    Respect env when deploying admin

--- deploy-updates/deploy-admin.sh
+++ deploy-updates/deploy-admin.sh
@@ -8,5 +8,5 @@ fi
 ENV=$1
 
 export ANSIBLE_VAULT_PASSWORD_FILE=.vault_pass.txt
-ansible-playbook  -i ../hosts/test.hosts -l main --user root  playbooks/update-admin-api/update-admin-api.yml
-ansible-playbook  -i ../hosts/test.hosts -l main --user root  playbooks/update-admin-client/update-admin-client.yml
+ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/update-admin-api/update-admin-api.yml
+ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/update-admin-client/update-admin-client.yml
