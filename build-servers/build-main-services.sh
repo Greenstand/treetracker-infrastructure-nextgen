5c44ebcad4c4e18f8a48b42de45e3bddcd56b712
Zaven <matthew.fullstackgis@gmail.com>
2020-07-21 14:43:02 -0700

    Set up the mobile api legacy service

--- build-servers/build-main-services.sh
+++ build-servers/build-main-services.sh
@@ -12,4 +12,5 @@ ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/install-
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/install-dependencies/install-dependencies.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/setup-map-api-service/setup-map-api-service.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/setup-admin-api-service/setup-admin-api-service.yml
+ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/setup-mobile-api-legacy-service/setup-mobile-api-legacy-service.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/install-nginx/install-nginx.yml
