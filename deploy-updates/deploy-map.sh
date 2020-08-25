a1978c26fe70b82a2dfdf59e0fc2077befecffd2
deanchen <antlet2004@gmail.com>
2020-08-26 06:02:47 +0800

    Added: set up vault pass file for web map

--- deploy-updates/deploy-map.sh
+++ deploy-updates/deploy-map.sh
@@ -7,5 +7,12 @@ then
 fi
 ENV=$1
 
+if [ "$ENV" = "prod" ]
+then
+  export ANSIBLE_VAULT_PASSWORD_FILE=.vault_pass_prod.txt
+else
+  export ANSIBLE_VAULT_PASSWORD_FILE=.vault_pass.txt
+fi
+
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/update-web-map-api/update-web-map-api.yml
 ansible-playbook  -i ../hosts/$ENV.hosts -l main --user root  playbooks/update-web-map-client/update-web-map-client.yml
